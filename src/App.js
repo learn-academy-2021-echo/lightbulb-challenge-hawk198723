4533aeb2d70a3eac700fde5b99d714e412e4ec29
Jason Wang <jason@Jasons-MacBook-Pro.local>
2022-04-12 20:06:42 -0700

    init push

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react'
-import './App.css'
-import AddOrRemoveLightBox from './components/AddOrRemoveLightBox'
+import React, { Component } from "react";
+import "./App.css";
+import AddOrRemoveLightBox from "./components/AddOrRemoveLightBox";
 
 class App extends Component {
-
-
   render() {
     return (
       <>
-        <h1>Łight ₿ulb Challenge</h1>
+        <h1>Łight ₿ulb</h1>
         <AddOrRemoveLightBox />
       </>
-    )
+    );
   }
 }
 
-export default App
+export default App;
